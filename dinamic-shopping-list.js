9dafc943bfb479374faf956e431b7b01eee92d9e
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-03-16 21:56:32 +0300

    create responce()

--- dinamic-shopping-list.js
+++ dinamic-shopping-list.js
@@ -4,3 +4,29 @@ let list = document.querySelector('ul');
 let input = document.querySelector('input');
 let button = document.querySelector('button');
 
+// func
+
+function response() {
+    let inputValue = input.value;
+    input.value = '';
+    
+    let listItem = document.createElement('li');
+    let spanItem = document.createElement('span');
+    let buttonItem = document.createElement('button');
+
+    listItem.appendChild(spanItem);
+    listItem.appendChild(buttonItem);
+
+    spanItem.textContent = inputValue;
+    buttonItem.textContent = 'DELETE';
+
+    list.appendChild(listItem);
+
+    buttonItem.addEventListener('click', () => listItem.remove());
+
+    input.focus();
+}
+
+// event
+
+button.addEventListener('click', response);
