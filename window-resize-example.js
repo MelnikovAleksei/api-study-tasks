cdef3eb52610e19680eaec94a52f09055f449155
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-03-16 21:34:40 +0300

    add style and event listener

--- window-resize-example.js
+++ window-resize-example.js
@@ -4,3 +4,18 @@ const div = document.querySelector('div');
 
 let windowWidth = window.innerWidth;
 let windowHeight = window.innerHeight;
+
+// styles
+
+div.style.width = windowWidth + 'px' ;
+div.style.height = windowHeight + 'px';
+
+// add event
+
+window.addEventListener('resize', () => {
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+
+    div.style.width = windowWidth + 'px';
+    div.style.height = windowHeight + 'px';
+})
