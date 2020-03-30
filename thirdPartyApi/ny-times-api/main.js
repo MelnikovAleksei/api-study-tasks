d1cf111978f9b9bf6432687f410c946c4b53d32a
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-03-30 13:39:38 +0300

    created displayResults, prevPage, nextPage func

--- thirdPartyApi/ny-times-api/main.js
+++ thirdPartyApi/ny-times-api/main.js
@@ -21,6 +21,9 @@ nav.style.display = 'none';
 
 searchForm.addEventListener('submit', submitSearch);
 
+prevButton.addEventListener('click', prevPage);
+nextButton.addEventListener('click', nextPage);
+
 function submitSearch(e) {
 	pageNumber = 0;
 	fetchResults(e);
@@ -29,7 +32,7 @@ function submitSearch(e) {
 function fetchResults(e) {
 	e.preventDefault();
 
-	url += baseURL + '?api-key=' + apiKey + '&page=' + pageNumber + '&q=' + searchTerm.value + '&fq=document_type:("article")'; 
+	url = baseURL + '?api-key=' + apiKey + '&page=' + pageNumber + '&q=' + searchTerm.value + '&fq=document_type:("article")'; 
 
 	if (startDate.value !== '') {
 		url += '&begin_date=' + startDate.value;
@@ -39,19 +42,96 @@ function fetchResults(e) {
 		url += '&end_date=' + endDate.value;
 	}
 
+	console.log('url: ' + url)
 	fetch(url)
-		.then((response) => {
-			return response.json();
+		.then((result) => {
+			return result.json();
 		})
 		.then((json) => {
-			let news = json;
-			console.log(news);
+			displayResults(json)
+			console.log(json);
 		})
 		.catch((e) => {
 			console.log(e);
 		})
 }
 
+function displayResults(json) {
+	while (section.firstChild) {
+		section.removeChild(section.firstChild)
+	}
 
+	const articles = json.response.docs;
+
+	if (articles.length === 10) {
+		nav.style.display = 'block';
+	} else {
+		nav.style.display = 'none';
+	}
+
+	if (articles.length === 0) {
+		const paragraph = document.createElement('p');
+		paragraph.textContent = 'No results returned';
+		section.appendChild(paragraph);
+		console.log('No results returned')
+	} else {
+		for (let i = 0; i < articles.length; i++) {
+			const article = document.createElement('article');
+			const h2 = document.createElement('h2');
+			const link = document.createElement('a');
+			const image = document.createElement('img');
+			const paragraph1 = document.createElement('p');
+			const paragraph2 = document.createElement('p');
+			const clearFix = document.createElement('div');
+
+			h2.textContent = articles[i].headline.main;
+			
+			link.href = articles[i].web_url;
+			link.textContent = articles[i].headline.main;
+			link.setAttribute('target', '_blank');
+
+			if (articles[i].multimedia.length > 0) {
+				image.src = 'http://www.nytimes.com/' + articles[i].multimedia[0].url;
+				image.alt = articles[i].headline.main;
+			}
+
+			paragraph1.textContent = articles[i].snippet;
+
+			paragraph2.textContent = 'Keywords: ';
+
+			for (let j = 0; j < articles[i].keywords.length; j++) {
+				let span = document.createElement('span');
+				span.textContent = articles[i].keywords[j].value;
+				paragraph2.appendChild(span);
+			}
+
+			clearFix.setAttribute('class', 'clearFix');
+
+			article.appendChild(h2);
+			article.appendChild(link);
+			article.appendChild(image);
+			article.appendChild(paragraph1);
+			article.appendChild(paragraph2);
+			article.appendChild(clearFix);
+			section.appendChild(article);
+		}
+	}
+}
+
+function prevPage (e) {
+	if (pageNumber > 0) {
+		pageNumber--;
+	} else {
+		return
+	}
+
+	fetchResults(e);
+}
+
+function nextPage (e) {
+	pageNumber++;
+
+	fetchResults(e);
+}
 
 
