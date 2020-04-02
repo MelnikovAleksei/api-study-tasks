c782a5d3b68d9bd08576f68fc3752301108dd9f4
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-04-02 13:51:05 +0300

    The HTMLMediaElement API
    created playPause, stopMedia, mediaBackward, mediaForward, setTime func

--- drawing-graphics/canvas/2_canvas_drawApp/2_canvas_drawApp_main.js
+++ drawing-graphics/canvas/2_canvas_drawApp/2_canvas_drawApp_main.js
@@ -1,31 +1,39 @@
 const canvas = document.querySelector('canvas');
 const width = canvas.width = window.innerWidth;
 const heght = canvas.height = window.innerHeight;
-const clearButton = document.querySelector('button');
+const clearButton = document.querySelector('.clearButton');
+const cancelButton = document.querySelector('.cancelButton');
+const returnButton = document.querySelector('.returnButton');
+
 let paragraph = document.querySelector('span');
 
 let output = document.querySelector('.output');
-let colorPicker = document.querySelector('input[type="color"]');
+let colorPicker = document.querySelector('.penColor');
 let sizePicker = document.querySelector('input[type="range"]');
-
+let backgroundColor = document.querySelector('.backgroundColor')
 let curX, curY;
 let pressed = false;
 
+let curXYArray = [];
+
+let ctxSave = [];
 let rect = canvas.getBoundingClientRect();
 
 const ctx = canvas.getContext('2d');
-
-ctx.fillStyle = 'white';
+ctx.fillStyle = backgroundColor.value;
 ctx.fillRect(0, 0, width, heght);
 
 function degToRad(degrees) {
     return degrees * Math.PI / 180;
 }
 
-function getMouseCoordinate(e) {
+canvas.onmousemove = function(e) {
     curX = e.clientX - rect.left;
     curY = e.clientY - rect.top;
     paragraph.textContent = 'x: ' + curX + ' | ' + 'y: ' + curY;
+    /*if (pressed) {
+        curXYArray.push('x:' + curX + 'y:' + curY + 'bgCol:' + backgroundColor.value + 'sP:' + sizePicker.value);
+    }*/
 }
 
 function draw() {
@@ -34,21 +42,46 @@ function draw() {
         ctx.beginPath();
         ctx.arc(curX, curY, sizePicker.value, degToRad(0), degToRad(360), false);
         ctx.fill();
+        //curXYArray.push('x:' + curX + 'y:' + curY + 'bgCol:' + backgroundColor.value + 'sP:' + sizePicker.value);
     }
     requestAnimationFrame(draw);
 }
 
 function clearCanvas() {
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = backgroundColor.value;
     ctx.fillRect(0, 0, width, heght);
 }
+/*
+function cancelAction() {
+    for (let i = 0; i < curXYArray.length; i++) {
+        const element = curXYArray[i];
+        let x = element.slice(element.indexOf('x:') + 2, element.indexOf('y:'));
+        let y = element.slice(element.indexOf('y:') + 2, element.indexOf('bgCol'));
+        let size = element.slice(element.indexOf('sP:') + 3, element.length);
+        let bgCol = element.slice(element.indexOf('bgCol:') + 6, element.indexOf('s'));
+        
+        size += 2
+        ctx.fillStyle = bgCol;
+        ctx.beginPath();
+        ctx.arc(x, y, size, degToRad(0), degToRad(360), false);
+        ctx.fill();      
+    }
+    
+    curXYArray = [];
+}
 
+function returnAction() {
+    
+}
+*/
 sizePicker.addEventListener('input', () => {
     output.textContent = sizePicker.value;
 })
 
 canvas.addEventListener('mousedown', () => {
     pressed = true;
+    //curXYArray = []
+
     paragraph.textContent = 'x: ' + curX + ' | ' + 'y: ' + curY + ' | ' + 'Pressed: ' + pressed;
 })
 
@@ -57,8 +90,17 @@ canvas.addEventListener('mouseup', () => {
     paragraph.textContent = 'x: ' + curX + ' | ' + 'y: ' + curY + ' | ' + 'Pressed: ' + pressed;
 })
 
-canvas.addEventListener('mousemove', getMouseCoordinate);
+
+backgroundColor.addEventListener('input', () => {
+    ctx.fillStyle = backgroundColor.value;
+    ctx.fillRect(0, 0, width, heght);
+})
 
 clearButton.addEventListener('click', clearCanvas);
+
+//cancelButton.addEventListener('click', cancelAction);
+
+//returnButton.addEventListener('click', returnAction)
+
 
 draw()
